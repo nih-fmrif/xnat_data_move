aab9a0cc21db8fe7ea30d51d4de5ae3984be0e3a
V. R. <for_usenet@yahoo.com>
2026-05-08 14:35:45 -0400

    This commit readies the prearchive_search_and_assign module to accept a list of session IDs, and assign those to a project. Right now - it is still ONLY printing what it would do, and not actually changing the project assigned to any session yet. That should come after receiving data and testing for the next commit.

--- prearchive_search_and_assign.py
+++ prearchive_search_and_assign.py
@@ -17,17 +17,37 @@ import  pandas as pd
 with xnat.connect('https://fmrif-xnat.nimh.nih.gov', user='xnat_admin_user') as session:
 # enter password for admin user when prompted.
 
-   prearchive_sessions = session.prearchive.sessions()
+   # Without the project='Unassigned' arguement to this function, this would normally
+   # return all sessions in PreArchive, i.e. including those already assigned to a 
+   # project.
+   prearchive_sessions_unassigned = session.prearchive.sessions(project='Unassigned')
 
-   for idx,expt in enumerate(prearchive_sessions):
+   # read in CSV with session IDs
+   session_ids = pd.read_csv('Path_to_CSV_of_needed_session_IDs.csv', skipinitialspace=True)
+
+   found_session_counter = 0
+
+   for each_session in prearchive_sessions_unassigned:
+
       try:
-         # print("session: %s, first scan: %s" % (str(expt), list(expt.scans.values())[0]))
-         first_scan = list(expt.scans.values())[0]
-         session_id = first_scan.dicom_dump(fields=['AccessionNumber'])[0]['value']
-         if (str(session_id) != ''):
-            print("session id: %s" % str(session_id))
+         first_scan     = list(each_session.scans.values())[0]
+         dcm_session_id = first_scan.dicom_dump(fields=['AccessionNumber'])[0]['value']
+
+         # Now iterate through list of sessions IDs to do 'soft matching'
+         for each_id in session_ids['label']:
+
+            if ((each_id in (str(each_session))) and (each_id == dcm_session_id)):
+               print("*** Found %s in %s !!! ***" % (str(each_id), str(each_session)))
+               # can execute project assignment here, with something like:
+               # each_session.move('ID of destination project here')
+               found_session_counter += 1
+            else:
+               # print("%s not found in %s" % (each_id, str(each_session)))
+               pass
       except xnat.exceptions.XNATResponseError:
          pass
 
-   session.close()
+   print ("Number of sessions found and assigned to project is %d" % found_session_counter)
+
+   session.disconnect()
 
